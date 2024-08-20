22122b4f732a9390dd8b15853b76054c9c9895e1
EricNorrwing <Eric.Norrwing@gmail.com>
2024-08-20 08:50:01 +0200

    First commit

---
+++ src/main/java/com/ericnorrwing/wsj_lek_1/WsjLek1Application.java
@@ -0,0 +1,14 @@
+package com.ericnorrwing.wsj_lek_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsjLek1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsjLek1Application.class, args);
+    }
+
+    //THis is an empty project to reopen later potentially
+}
---
+++ src/test/java/com/ericnorrwing/wsj_lek_1/WsjLek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericnorrwing.wsj_lek_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsjLek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
